00ab10a2e90df6c211d149cc126bf4487a03a9c2
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2013-10-19 17:13:45 +0200

    Scaffold for model Post added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsAngularBowerHeroku::Application.routes.draw do
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
