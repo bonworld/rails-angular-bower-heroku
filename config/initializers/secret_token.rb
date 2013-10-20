4f5ca7bb4acdf13b13fbcad49aa9862dae2d3e2b
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2013-10-20 14:25:42 +0200

    Make secret token available for Heroku.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAngularBowerHeroku::Application.config.secret_key_base = '97b6bf05d486a2c83787b5aa9ee83b46cab1c66b0cf359e7d666d5ff1877c39c546aef0d1880d2babf49528ba04022ef4472ae804ee0e549f6e4524aa6a610cb'
