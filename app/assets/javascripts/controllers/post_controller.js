919ddfe4ddfd52c0293e680aaeef695f4adf71b9
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2013-10-27 20:30:02 +0100

    Remove obsolete stuff.

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -3,13 +3,6 @@
 RabhApp.controller('PostsController', ['$scope', 'PostModel', '$filter',
   function PostsController($scope, PostModel, $filter) {
     $scope.posts = PostModel.query();
- /*   $scope.filteredPosts = [];
-   
-    $scope.filterPosts = function() {
-      $scope.filteredPosts = filterFilter($scope.posts, $scope.query);
-    };
-
-    $scope.$watch('query', $scope.filterPosts); */
   }
 ]);
 
