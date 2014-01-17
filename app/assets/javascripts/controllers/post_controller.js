462e3121407a349ec6cb35ebf84bc4716e4ebf90
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2014-01-17 18:31:22 +0100

    Revert the change
    ngmin does not work if using variables, only with
    „angular.module(….“

--- app/assets/javascripts/controllers/post_controller.js
+++ app/assets/javascripts/controllers/post_controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
-RabhApp.controller('PostsController', 
+RabhApp.controller('PostsController', ['$scope', 'PostModel', '$filter',
   function PostsController($scope, PostModel, $filter) {
     $scope.posts = PostModel.query();
   }
-);
+]);
 
-RabhApp.controller('SinglePostController', 
+RabhApp.controller('SinglePostController', ['$scope', '$routeParams', 'PostModel',
   function SinglePostController($scope, $routeParams, PostModel) {
     $scope.post = PostModel.get({'post_id': $routeParams.postId});
   }
-);
+]);
